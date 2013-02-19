2b3659bfa572a3965fe5c804e518690d3159d5c3
Aditya Ranjan <aranjan@twitter.com>
2013-02-19 13:56:30 -0800

    Made a minor change

--- adi/bonding.py
+++ adi/bonding.py
@@ -1,8 +1,9 @@
 #! /usr/bin/python2.7
 
-import smtplib
 import urllib2
 import json
+import string
+import socket
 import string
 import subprocess
 
@@ -15,5 +16,10 @@ all_hosts= json.load(obj)
 for mc in all_hosts:
 	hostnames = all_hosts[mc]['facts']['hostname']['server']
 	print hostnames
-	bond_status= urllib2.urlopen('http://%s:30000/system/network/if/eth0/bonding/status'%hostnames).read()
-	print bond_status
+	try:
+		bond_status= urllib2.urlopen('http://%s:30000/system/network/if/eth0/bonding/status'%hostnames,timeout=2).read()
+		print bond_status
+	except:
+		print hostnames, 'FAIL'
+
+
