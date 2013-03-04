841bdbfaac8330a7e07057a8d36193c763d7ff4e
Aditya Ranjan <aranjan@twitter.com>
2013-03-04 08:31:42 -0800

    New modifications

--- adi/bonding_030413.py
+++ adi/bonding_030413.py
@@ -22,6 +22,20 @@ else:
 	exit
 '''
 
+parser = OptionParser()
+parser.add_option("-D","--Datcenter",help="select the datacenter")
+(options, args) = parser.parse_args()
+
+if args == 'smf1':
+	obj= urllib2.urlopen('http://audubon.smf1.twitter.com/mdb/api/server.json?')
+elif args == 'atla':
+	obj= urllib2.urlopen('http://audubon.atla.twitter.com/mdb/api/server.json?')
+elif args == 'smfd':
+	obj= urllib2.urlopen('http://audubon.smfd.twitter.com/mdb/api/server.json?')
+else:
+	print " You did not enter a valid Datacenter "
+	exit
+
 
 def get_bonding_status(machine):
         try:
@@ -31,15 +45,19 @@ def get_bonding_status(machine):
                         print machine, bond_status
         except:
                 print machine, 'Vex Exception'
+
+white_list = ['undefined', 'hbase-v2-worker', 'hadoop', 'hadoop-dw-worker','mesos','cassandra.prod.cuckoo-minutely','manhattan.test.greenbean','hadoop-tst-worker','manhattan.prod.spare-d', 'cuckoo-cassandra','hadoop-exp2','mesos.multi.slave','cassandra.prod.cuckoo-minutely','ads-memcache','ads.memcache.staging','cassandra.prod.chubbysnipe','cassandra.prod.reserve','cassandra.prod.spare','cassandra.prod.spiderduck','cassandra.prod.tweetdeck','hadoop','hadoop-dw-worker','hadoop-photo-worker','hadoop-tst-worker','manhattan.prod.revenue','manhattan.prod.spare-d']
+
 '''
+All the URL's for different datcenters
+ 
 obj= urllib2.urlopen('http://audubon.smf1.twitter.com/mdb/api/server.json?')
 obj= urllib2.urlopen('http://audubon.atla.twitter.com/mdb/api/server.json?') 
 obj= urllib2.urlopen('http://audubon.smfd.twitter.com/mdb/api/server.json?')
 '''
 
-white_list = ['undefined', 'hbase-v2-worker', 'hadoop', 'hadoop-dw-worker','mesos','cassandra.prod.cuckoo-minutely','manhattan.test.greenbean','hadoop-tst-worker','manhattan.prod.spare-d', 'cuckoo-cassandra','hadoop-exp2','mesos.multi.slave','cassandra.prod.cuckoo-minutely','ads-memcache','ads.memcache.staging','cassandra.prod.chubbysnipe','cassandra.prod.reserve','cassandra.prod.spare','cassandra.prod.spiderduck','cassandra.prod.tweetdeck','hadoop','hadoop-dw-worker','hadoop-photo-worker','hadoop-tst-worker','manhattan.prod.revenue','manhattan.prod.spare-d']
+#obj= urllib2.urlopen('http://audubon.atla.twitter.com/mdb/api/server.json?')
 
-obj= urllib2.urlopen('http://audubon.atla.twitter.com/mdb/api/server.json?')
 all_hosts= json.load(obj)
 machines = []
 for mc in all_hosts:
